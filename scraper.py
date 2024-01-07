cb592f4119b6c135ebbf357c58a3cbce79710b7e
Nitanta Adhikari <nitanta.adh007@gmail.com>
2024-01-07 20:59:31 +0545

    WIP: Scraping and crawling

--- scraper.py
+++ scraper.py
@@ -1,17 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
 from collections import Counter
 import matplotlib.pyplot as plt
-from nltk.tokenize import word_tokenize
-from nltk.stem import SnowballStemmer
+import matplotlib.font_manager as fm
 from urllib.parse import urlparse, urljoin
 from nepalitokenizers import WordPiece
 # importing from the HuggingFace tokenizers package
 from tokenizers.processors import TemplateProcessing
 
 # keywords = ["समाचार", "आम निर्वाचन २०७९", "विचार", "राजनीति"]
-keywords = ["समाचार"]
+keywords = ["राजनीति"]
+
 
 def setopati(query):
     # Search from 2023 to 2024
@@ -134,51 +133,34 @@ def preprocess_text(text):
     tokenizer_sp.post_processor = TemplateProcessing()
     tokens = tokenizer_sp.encode(text)
 
-    print(tokens.tokens)
+    words = tokens.tokens
 
     stop_words = load_stop_words('nepali.txt')
-    print(stop_words)
-    # words = [word for word in words if word.lower() not in stop_words]
+    words = [word for word in words if word.lower() not in stop_words]
 
     # stemmer = SnowballStemmer('nepali')
     # words = [stemmer.stem(word) for word in words]
-
-    # return words
+    print(words)
+    return words
 
 def compute_term_frequencies(words):
-    # Compute term frequencies
     term_frequencies = Counter(words)
     return term_frequencies
 
 def plot_term_frequencies(term_frequencies, genre):
-    # Plot the top 20 terms
     common_terms = term_frequencies.most_common(20)
     terms, counts = zip(*common_terms)
     
+    property = fm.FontProperties(fname='Nirmala.ttf')
     plt.barh(terms, counts, color='skyblue')
-    plt.xlabel('Term Frequency')
-    plt.title(f'Top 20 Terms in {genre}')
+    plt.xlabel('Term Frequency', fontproperties=property)
+    plt.title(f'Top 20 Terms in {genre}', fontproperties=property)
+    plt.yticks(fontproperties=property)
     plt.show()
-    
-    # for article in articles:
-    #     # Assuming 'text' is the key for the article text
-    #     words = preprocess_text(article['text'])
-        
-    #     for keyword in keywords:
-    #         if keyword in words:
-    #             # Do something with the article, e.g., analyze sentiment
-
-    #     term_frequencies = compute_term_frequencies(words)
-    #     plot_term_frequencies(term_frequencies, genre='News')  # Replace 'News' with the actual genre
 
 # Implement similar logic for political leaders and parties analysis
 
-# for query in keywords:
-#     data = fetch_data(query)
-#     result_string = ' '.join([item["title"] for item in data])
-#     preprocessed_string = preprocess_text(result_string)
-#     print(preprocessed_string)
-    
-
-stop_words = load_stop_words('nepali.txt')
-print(stop_words)
+for query in keywords:
+    data = fetch_data(query)
+    result_string = ' '.join([item["title"] for item in data])
+    plot_term_frequencies(compute_term_frequencies(preprocess_text(result_string)), query)
